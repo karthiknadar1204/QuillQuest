ed015e3b9ef8f67a8b696a96936e6d29c58c8158
karthik <karthiknadar1204@gmail.com>
2024-01-28 09:33:57 +0530

    Added the create post api route

---
+++ api/routes/post.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import { create } from '../controllers/post.controller.js';
+
+const postRouter = express.Router();
+
+postRouter.post('/create', verifyToken, create)
+
+
+export default postRouter;
