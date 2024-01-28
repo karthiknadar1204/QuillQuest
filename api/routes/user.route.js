67edace2cc1475353ce07969bcb5dc871dee9b9b
karthik <karthiknadar1204@gmail.com>
2024-01-28 08:47:34 +0530

    compeleted signout user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -10,5 +10,6 @@ const userRouter = express.Router();
 userRouter.get('/test',test);
 userRouter.put('/update/:userId', verifyToken, updateUser);
 userRouter.delete('/delete/:userId', verifyToken, deleteUser);
+userRouter.post('/signout', signout);
 
 export default userRouter;
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -145,6 +145,24 @@ const DashProfile = () => {
     }
   };
 
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -240,7 +258,7 @@ const DashProfile = () => {
           Delete Account
         </span>
         <span
-          // onClick={handleSignout}
+          onClick={handleSignout}
           className="cursor-pointer"
         >
           Sign Out
