d34b9d77ed32f3c4baa47337d75355d2308d7eb2
karthik <karthiknadar1204@gmail.com>
2024-09-17 16:45:18 +0530

    asdf

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const app=express();
 
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173']
 }))
 
 app.use(bodyParser.json());
