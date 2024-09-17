b8a3618e320f520dcf367166f868fb32ddf64497
karthik <karthiknadar1204@gmail.com>
2024-09-17 17:50:49 +0530

    asdf

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -30,6 +30,7 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
+      console.log('dddd', data)
 
       if (data.success === false) {
         dispatch(signInFailure(data.message));
